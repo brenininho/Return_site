c57c47f901b33c831da66be22fbe28ed9a927f79
Pendragon <joaoricardoroma@gmail.com>
2021-10-15 21:25:53 -0300

    criado o template e os caminhos

--- come_back_site/manage_employees/views.py
+++ come_back_site/manage_employees/views.py
@@ -13,7 +13,7 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:2]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    output = ', '.join([p.question_text for p in latest_question_list])
     return HttpResponse(output)
 
 
