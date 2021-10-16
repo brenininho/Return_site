356ae7464c2c287d659cd093ed8c5130f05802cc
Breno <breno.valle9@hotmail.com>
2021-10-15 22:02:42 -0300

    resolvendo erro do template

--- come_back_site/manage_employees/views.py
+++ come_back_site/manage_employees/views.py
@@ -1,17 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Question
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
+
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:2]
-    output = ', '.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'manage_employees/index.html', context)
