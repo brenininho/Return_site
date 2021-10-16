4235ecf88cc094b8011ecbe2567913232b897739
Breno <breno.valle9@hotmail.com>
2021-10-16 14:20:13 -0300

    modificando o admin

--- come_back_site/manage_employees/views.py
+++ come_back_site/manage_employees/views.py
@@ -1,17 +1,27 @@
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'manage_employees/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'manage_employees/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'manage_employees/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'manage_employees/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'manage_employees/results.html'
 
 
 def vote(request, question_id):
@@ -27,11 +37,3 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('manage_employees:results', args=(question.id,)))
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'manage_employees/index.html', context)
