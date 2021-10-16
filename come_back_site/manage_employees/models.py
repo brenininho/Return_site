c4e28927b9220688df092e443df3b9b1b2a509cf
Pendragon <joaoricardoroma@gmail.com>
2021-10-16 19:07:59 -0300

    trocando os valores

--- come_back_site/manage_employees/models.py
+++ come_back_site/manage_employees/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('data publicada')
+class Employees(models.Model):
+    employee_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    cpf = models.IntegerField(default=11)
+    birthday_date = models.DateTimeField('data do aniversário')
 
     def __str__(self):
-        return self.question_text
+        return self.employee_name
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Employees, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
